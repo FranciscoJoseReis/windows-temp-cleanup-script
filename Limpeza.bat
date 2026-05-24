690b0ea73ee1f5b6fb0e8d372fff1e13e09afe12
FranciscoJoseReis <fjreis@outlook.com>
2026-05-24 11:53:51 +0100

    Update_v1.3

--- Limpeza.bat
+++ Limpeza.bat
@@ -1,10 +1,27 @@
 @echo off
-title Limpeza de Ficheiros Temporarios
 color 0A
+setlocal EnableExtensions
+
+:: ==============================
+:: DEFINIR LOG
+:: ==============================
+set "LOGFILE=%~dp0cleanup_log.txt"
+
+:: Apagar log anterior se existir
+if exist "%LOGFILE%" del /f /q "%LOGFILE%"
+
+echo ============================== >> "%LOGFILE%"
+echo Limpeza iniciada em %date% %time% >> "%LOGFILE%"
+echo ============================== >> "%LOGFILE%"
+
+echo.
+echo A iniciar limpeza...
+echo.
 
 :: ==============================
 :: VERIFICAR ADMIN
 :: ==============================
+
 net session >nul 2>&1
 if %errorLevel% neq 0 (
     echo Solicitando privilegios de administrador...
@@ -19,18 +36,6 @@ if %errorLevel% neq 0 (
     if exist "%temp%\getadmin.vbs" ( del "%temp%\getadmin.vbs" )
     pushd "%CD%"
     CD /D "%~dp0"
-:: ==============================
-:: LOG
-:: ==============================
-set LOGFILE=%~dp0cleanup_log.txt
-
-echo ============================== >> "%LOGFILE%"
-echo Limpeza iniciada em %date% %time% >> "%LOGFILE%"
-echo ============================== >> "%LOGFILE%"
-
-echo.
-echo A iniciar limpeza...
-echo.
 
 :: ==============================
 :: TEMP UTILIZADOR
@@ -52,11 +57,17 @@ del /s /f /q "%systemroot%\temp\*.*" >> "%LOGFILE%" 2>&1
 for /d %%p in ("%systemroot%\temp\*") do rd /s /q "%%p" >> "%LOGFILE%" 2>&1
 
 :: ==============================
+:: FINAL
+:: ==============================
 echo.
 echo Limpeza concluida com sucesso!
 echo ============================== >> "%LOGFILE%"
 echo Limpeza concluida em %date% %time% >> "%LOGFILE%"
 
+echo.
+echo Verificar log para detalhes:
+echo %LOGFILE%
+
 pause
-start %LOGFILE%
+start "" "%LOGFILE%"
 exit
